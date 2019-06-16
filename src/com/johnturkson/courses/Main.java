eb62c5b85856318e6304a1efb410e5c52fccfad8
John Turkson <github@johnturkson.com>
2019-06-15 17:56:14 -0700

    Added support for importing/exporting of Subjects, Courses, and Sections

--- src/com/johnturkson/courses/Main.java
+++
@@ -1,9 +0,0 @@
-package com.johnturkson.courses;
-
-public class Main {
-    public static void main(String... args) {
-        Subjects.getSubjects().forEach(s -> System.out.println(s.getCode() + ": " + s.getName() + " - " + s.getFaculty()));
-        Courses.getCourses("CPSC").forEach(System.out::println);
-        Sections.getSections("CPSC", "213").forEach(System.out::println);
-    }
-}
